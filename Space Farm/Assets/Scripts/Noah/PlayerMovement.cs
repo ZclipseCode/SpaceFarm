8868f5f0e249f30c208dcbe9836af0e2261da59f
Noah1625 <42361960+Noah1625@users.noreply.github.com>
2023-07-15 14:19:13 -0500

    Dodge

--- Space Farm/Assets/Scripts/Noah/PlayerMovement.cs
+++ Space Farm/Assets/Scripts/Noah/PlayerMovement.cs
@@ -9,8 +9,15 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     public bool isFacingRight;
+    public KeyCode dodgeButton = KeyCode.H;
+    public bool isDodging;
+    public float dodgeTime = 1;
 
     Vector2 movement;
+
+    public float dodgeForce = 5;
+
+
 
     // Update is called once per frame
     void Update()
@@ -24,11 +31,22 @@ public class PlayerMovement : MonoBehaviour
         //animator.SetFloat("Horizontal", movement.x);
         //animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (Input.GetKeyDown(dodgeButton) && !isDodging)
+        {
+            StartCoroutine(Dodge());
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (!isDodging)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * dodgeForce * Time.fixedDeltaTime);
+        }
     }
 
     void Flip()
@@ -37,5 +55,14 @@ public class PlayerMovement : MonoBehaviour
         Vector3 localScale = transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
+    }
+
+    IEnumerator Dodge()
+    {
+        isDodging = true;
+        
+        yield return new WaitForSeconds(dodgeTime);
+
+        isDodging = false;
     }
 }
