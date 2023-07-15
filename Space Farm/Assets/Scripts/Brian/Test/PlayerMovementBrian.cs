ae3bf8a779cd98105c4217f045d1d47d4aba5f20
ZclipseCode <bmichaelbusiness@gmail.com>
2023-07-15 14:18:11 -0500

    test dodge

---
+++ Space Farm/Assets/Scripts/Brian/Test/PlayerMovementBrian.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBrian : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    //public Animator animator;
+    public bool isFacingRight;
+
+    Vector2 movement;
+
+    public float dodgeForce = 5;
+    public KeyCode dodgeButton = KeyCode.H;
+    bool isDodging;
+    public float dodgeTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (!isFacingRight && movement.x > 0 || isFacingRight && movement.x < 0)
+        {
+            Flip();
+        }
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKeyDown(dodgeButton) && !isDodging)
+        {
+            StartCoroutine(Dodge());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isDodging)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * dodgeForce * Time.fixedDeltaTime);
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    IEnumerator Dodge()
+    {
+        isDodging = true;
+
+        yield return new WaitForSeconds(dodgeTime);
+
+        isDodging = false;
+    }
+}
